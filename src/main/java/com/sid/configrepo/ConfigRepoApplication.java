52c0b1fcdbd0c5deb15f50115cb119c0cc39e806
Arsene20 <arsenedamlabin@gmail.com>
2022-11-18 02:57:53 +0100

    update

---
+++ src/main/java/com/sid/configrepo/ConfigRepoApplication.java
@@ -0,0 +1,13 @@
+package com.sid.configrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigRepoApplication.class, args);
+	}
+
+}
